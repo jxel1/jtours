385ed8024f868c828947c437851857d7a8fb4f95
Joel1b <bitrijoel@gmail.com>
2024-08-02 14:26:22 +0200

    SF

--- server.js
+++ server.js
@@ -8,18 +8,21 @@ const app = require('./app');
 const port = process.env.PORT || 3000;
 
 mongoose
-  .connect(process.env.DATABASE)
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
-    console.log('Connected!');
+    console.log('Connected to MongoDB!');
   })
   .catch((err) => {
-    console.error('Connection error', err);
+    console.error('Connection error:', err.message);
   });
 
 const server = http.createServer(app);
 
-server.listen(port, `0.0.0.0`, () => {
-  console.log(`App listening on port ${port}`);
+server.listen(port, '0.0.0.0', () => {
+  console.log(`App running on port ${port}...`);
 });
 
 process.on('unhandledRejection', (err) => {
