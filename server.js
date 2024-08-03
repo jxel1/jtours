e336b67eee873071fc7ee45fefb96f312b270633
Joel1b <bitrijoel@gmail.com>
2024-08-03 12:04:34 +0200

    Port binding

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 mongoose
   .connect(process.env.DATABASE)
