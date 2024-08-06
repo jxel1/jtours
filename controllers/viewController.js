2008903b5970ec3ed0e5f658900dd62d595a0e48
Joel1b <bitrijoel@gmail.com>
2024-08-06 13:41:24 +0200

    Media Responsivity

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -5,11 +5,8 @@ const AppError = require('../utils/appError');
 const Booking = require('../models/bookingModel');
 
 exports.getOverview = catchAsync(async (req, res, next) => {
-  // 1) Get tour data from collection
   const tours = await Tour.find();
 
-  // 2) Build template
-  // 3) Render that template using tour data from 1)
   res.status(200).render('overview', {
     title: 'All Tours',
     tours,
@@ -17,7 +14,6 @@ exports.getOverview = catchAsync(async (req, res, next) => {
 });
 
 exports.getTour = catchAsync(async (req, res, next) => {
-  // 1) Get the data, for the requested tour (including reviews and guides)
   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
     path: 'reviews',
     fields: 'review rating user',
--- public/js/index.js
+++ public/js/index.js
@@ -1,86 +1,92 @@
-// import '@babel/polyfill';
-// import { login, logout, signup, forgotPassword } from './login';
-// import { displayMap } from './mapbox';
-// import { updateData } from './updateData';
+import '@babel/polyfill';
+import { login, logout, signup, forgotPassword } from './login';
+import { displayMap } from './mapbox';
+import { updateData } from './updateData';
 
-// const mapBox = document.getElementById('map');
-// const loginForm = document.querySelector('.form-login');
-// const logoutBtn = document.querySelector('.nav__el--logout');
-// const userDataForm = document.querySelector('.form-user-data');
-// const userPasswordForm = document.querySelector('.form-user-password');
-// const resetPasswordForm = document.querySelector('.form-password-reset');
-// const signupForm = document.querySelector('.form-signup');
+const mapBox = document.getElementById('map');
+const loginForm = document.querySelector('.form-login');
+const logoutBtn = document.querySelector('.nav__el--logout');
+const userDataForm = document.querySelector('.form-user-data');
+const userPasswordForm = document.querySelector('.form-user-password');
+const resetPasswordForm = document.querySelector('.form-password-reset');
+const signupForm = document.querySelector('.form-signup');
+const logoutBtn2 = document.querySelector('.nav__el--logout2');
 
-// if (signupForm) {
-//   document.querySelector('.form').addEventListener('submit', (e) => {
-//     e.preventDefault();
-//     console.log('hello there');
-//     const name = document.getElementById('name').value;
-//     const email = document.getElementById('email').value;
-//     const password = document.getElementById('password').value;
-//     const passwordConfirm = document.getElementById('passwordConfirm').value;
-//     console.log(name + email + password + passwordConfirm);
-//     signup(name, email, password, passwordConfirm);
-//   });
-// }
+if (signupForm) {
+  document.querySelector('.form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log('hello there');
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('passwordConfirm').value;
+    console.log(name + email + password + passwordConfirm);
+    signup(name, email, password, passwordConfirm);
+  });
+}
 
-// if (mapBox) {
-//   const locations = JSON.parse(document.getElementById('map').dataset.locations);
-//   displayMap(locations);
-// }
+if (mapBox) {
+  const locations = JSON.parse(document.getElementById('map').dataset.locations);
+  displayMap(locations);
+}
 
-// if (loginForm) {
-//   document.querySelector('.form').addEventListener('submit', (e) => {
-//     e.preventDefault();
-//     const email = document.getElementById('email').value;
-//     const password = document.getElementById('password').value;
-//     login(email, password);
-//   });
-// }
+if (loginForm) {
+  document.querySelector('.form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    login(email, password);
+  });
+}
 
-// if (logoutBtn) {
-//   logoutBtn.addEventListener('click', logout);
-// }
-// if (userDataForm)
-//   userDataForm.addEventListener('submit', async (e) => {
-//     e.preventDefault();
-//     const form = new FormData();
-//     form.append('name', document.getElementById('name').value);
-//     form.append('email', document.getElementById('email').value);
-//     form.append('photo', document.getElementById('photo').files[0]);
-//     console.log(form);
+if (logoutBtn) {
+  logoutBtn.addEventListener('click', logout);
+}
 
-//     // const name = document.getElementById('name').value;
-//     // const email = document.getElementById('email').value;
-//     // const photo = document.getElementById('photo').files[0];
+if (logoutBtn2) {
+  logoutBtn2.addEventListener('click', logout);
+}
 
-//     await updateData(form, 'data');
-//   });
+if (userDataForm)
+  userDataForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const form = new FormData();
+    form.append('name', document.getElementById('name').value);
+    form.append('email', document.getElementById('email').value);
+    form.append('photo', document.getElementById('photo').files[0]);
+    console.log(form);
 
-// if (userPasswordForm)
-//   userPasswordForm.addEventListener('submit', async (e) => {
-//     e.preventDefault();
-//     document.querySelector('.btn--save-password').textContent = 'Updating...';
+    // const name = document.getElementById('name').value;
+    // const email = document.getElementById('email').value;
+    // const photo = document.getElementById('photo').files[0];
 
-//     const passwordCurrent = document.getElementById('password-current').value;
-//     const password = document.getElementById('password').value;
-//     const passwordConfirm = document.getElementById('password-confirm').value;
-//     await updateData({ passwordCurrent, password, passwordConfirm }, 'password');
+    await updateData(form, 'data');
+  });
 
-//     document.querySelector('.btn--save-password').textContent = 'Save password';
-//     document.getElementById('password-current').value = '';
-//     document.getElementById('password').value = '';
-//     document.getElementById('password-confirm').value = '';
-//   });
+if (userPasswordForm)
+  userPasswordForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    document.querySelector('.btn--save-password').textContent = 'Updating...';
 
-// if (resetPasswordForm) {
-//   document.querySelector('.form').addEventListener('submit', async (e) => {
-//     e.preventDefault();
-//     const email = document.getElementById('email').value;
+    const passwordCurrent = document.getElementById('password-current').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('password-confirm').value;
+    await updateData({ passwordCurrent, password, passwordConfirm }, 'password');
 
-//     document.getElementById('hid').hidden = false;
-//     console.log(email, '1');
-//     await forgotPassword(email);
-//     console.log(email, '2');
-//   });
-// }
+    document.querySelector('.btn--save-password').textContent = 'Save password';
+    document.getElementById('password-current').value = '';
+    document.getElementById('password').value = '';
+    document.getElementById('password-confirm').value = '';
+  });
+
+if (resetPasswordForm) {
+  document.querySelector('.form').addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+
+    document.getElementById('hid').hidden = false;
+    console.log(email, '1');
+    await forgotPassword(email);
+    console.log(email, '2');
+  });
+}
